268160c171a020b459e172ab9be5fcf9b50c8582
Vlad Popa <1930022@champlaincollege.qc.ca>
2020-10-17 12:23:40 -0400

    Add project files.

---
+++ BankAccountApplication/Account.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountApplication
+{
+    abstract class Account : IAccount
+    {
+        private double startBalance;
+        private double currentBalance;
+
+        protected double totalDeposits;
+        protected int depositCount;
+
+        protected double totalWithdrawals;
+        protected int withdrawalCount;
+
+        protected double annualInterest;
+        protected double monthServiceCharge;
+
+
+        public double MonthStartingBalance
+        {
+            get
+            {
+                return startBalance;
+            }
+        }
+
+        public double MonthCurrentBalance
+        {
+            get
+            {
+                return currentBalance;
+            }
+        }
+
+        public enum AccountStatus
+        {
+            active = 1,
+            inactive = 0
+        }
+
+
+        public Account(double bal, double int_rate)
+        {
+            startBalance = bal;
+            currentBalance = bal;
+            annualInterest = int_rate;
+        }
+
+        public void CalculateInterest()
+        {
+            double monthInterest = annualInterest / 12;
+            monthInterest = currentBalance * monthInterest;
+            currentBalance += monthInterest;
+        }
+
+        public double PercentChange()
+        {
+            double per_diff = Math.Round(((currentBalance - startBalance) / startBalance), 2);
+            return per_diff;
+        }
+
+        public virtual string CloseAndReport()
+        {
+            currentBalance -= monthServiceCharge;
+            CalculateInterest();
+
+            double monthInterest = annualInterest / 12;
+            string formattedIntRate = string.Format("{0:0.00}%",monthInterest * 100);
+            double intAmount = currentBalance * monthInterest;
+            string formattedIntAmount = string.Format("{0:C}", intAmount);
+
+            string balance = "Starting balance: " + string.Format("{0:C}", startBalance) + 
+                "\nCurrent Balance: " + String.Format("{0:C}", currentBalance);
+
+            string percentage = "Percent difference between starting and current balance: " + PercentChange();
+            string intPay = "Month's interest rate: (" + formattedIntRate + ") paid: " + formattedIntAmount;
+            string serviceCharge = " Monthly Service charge paid: " + string.Format("{0:C}", monthServiceCharge);
+
+            string str = string.Format("{0}\n{1}\n{2}\n{3}\n", balance,percentage,intPay,serviceCharge);
+
+            withdrawalCount = 0;
+            depositCount = 0;
+
+            totalWithdrawals = 0;
+            totalDeposits = 0;
+
+            monthServiceCharge = 0;
+
+            return str;
+
+        }
+
+
+        public virtual void MakeDeposit(double amount)
+        {
+            currentBalance += amount;
+            totalDeposits += amount;
+            depositCount++;
+        }
+
+        public virtual void MakeWithDrawal(double amount)
+        {
+            currentBalance -= amount;
+            totalWithdrawals += amount;
+            withdrawalCount++;
+        }
+    }
+}
---
+++ BankAccountApplication/SavingsAccount.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountApplication
+{
+    class SavingsAccount : Account, IAccount
+    {
+        AccountStatus stat;
+        public SavingsAccount(double bal, double int_rate): base(bal, int_rate)
+        {
+            if(bal < 25)
+            {
+                stat = AccountStatus.inactive;
+            }
+            else
+            {
+                stat = AccountStatus.active;
+            }
+        }
+
+        public override string CloseAndReport()
+        {
+            if (withdrawalCount > 4)
+            {
+                monthServiceCharge += (withdrawalCount - 4) * 1;
+                return base.CloseAndReport();
+            }
+            else
+            {
+                return base.CloseAndReport();
+            }
+            
+        }
+
+        public override void MakeDeposit(double amount)
+        {
+            if(stat == AccountStatus.inactive && MonthCurrentBalance + amount > 25)
+            {
+                base.MakeDeposit(amount);
+                stat = AccountStatus.active;
+            }
+            else
+            {
+                base.MakeDeposit(amount);
+            }
+        }
+
+        public override void MakeWithDrawal(double amount)
+        {
+            if(stat == AccountStatus.inactive)
+            {
+                Console.WriteLine("This bank account is inactive");
+                Console.WriteLine("If you want to make a withdrawal, you must have a balance over $25");
+            }
+
+            else
+            {
+                if(MonthCurrentBalance - amount > 25)
+                {
+                    base.MakeWithDrawal(amount);
+                    Console.WriteLine(string.Format("{0:C}", amount) + " withdrawed successfully");
+                    stat = AccountStatus.inactive;
+                }
+            }
+        }
+    }
+}

e131690fdd77ece2c00c4b4f58853e0b72c8e68e
Vlad Popa <1930022@champlaincollege.qc.ca>
2020-10-17 12:53:24 -0400

    Added GlobalSavingsAccount class.

---
+++ BankAccountApplication/GlobalSavingsAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountApplication
+{
+    class GlobalSavingsAccount : SavingsAccount, IExchangeable, IAccount
+    {
+        public GlobalSavingsAccount(double bal, double int_rate) : base(bal, int_rate)
+        {
+
+        }
+
+        public double USValue(double rate)
+        {
+            return MonthCurrentBalance * rate;
+        }
+    }
+}

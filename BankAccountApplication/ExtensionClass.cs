1b1d29a3ea7073c914610c4dee63123660c49c23
Vlad Popa <1930022@champlaincollege.qc.ca>
2020-10-17 15:43:20 -0400

    Added the extension class

---
+++ BankAccountApplication/ExtensionClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountApplication
+{
+    public static class ExtensionClass
+    {
+        public static string NAMoneyFormat(this double n, Boolean b)
+        {
+            double money = n * 100;
+
+            if(b == true)
+            {
+                if (money < 0)
+                {
+                    money *= -1;
+                    string str = string.Format("{0:C}", (Decimal.Ceiling((decimal)money)) / 100); //rounds number
+                    return str;
+                }
+                else
+                {
+                    string str = string.Format("{0:C}", (Decimal.Ceiling((decimal)money)) / 100); //rounds number
+                    return str;
+                }
+            }
+            else
+            {
+                if (money < 0)
+                {
+                    money *= -1;
+                    string str = string.Format("{0:C}", (Decimal.Ceiling((decimal)money)) / 100); //rounds number
+                    return str;
+                }
+                else
+                {
+                    string str = string.Format("{0:C}", (Decimal.Ceiling((decimal)money)) / 100); //rounds number
+                    return str;
+                }
+            }
+        }
+
+        public static double NAMoneyFormatD(this double n, Boolean b) //Method to avoid discrepancies
+        {
+            double money = n * 100;
+            if (b == true)
+            {
+                if (money < 0)
+                {
+                    money *= -1;
+                    decimal d = (Decimal.Ceiling((decimal)money)) / 100;
+                    return ((double) d);
+                }
+                else
+                {
+                    decimal d = (Decimal.Ceiling((decimal)money)) / 100;
+                    return ((double)d);
+                }
+            }
+            else
+            {
+                if (money < 0)
+                {
+                    money *= -1;
+                    decimal d = (Decimal.Ceiling((decimal)money)) / 100;
+                    return ((double)d);
+                }
+                else
+                {
+                    decimal d = (Decimal.Ceiling((decimal)money)) / 100;
+                    return ((double)d);
+                }
+            }
+        }
+    }
+}
